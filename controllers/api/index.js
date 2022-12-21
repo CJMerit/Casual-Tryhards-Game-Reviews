bfaecf101fbbfd39259cb7eb606152aace988357
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2022-12-20 18:58:53 -0800

    Created models files and api files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
 
+const userRoutes = require("./userRoutes")
+const gameRoutes = require("./gameRoutes")
+const reviewRoutes = require("./reviewRoutes")
+
 module.exports = router;
