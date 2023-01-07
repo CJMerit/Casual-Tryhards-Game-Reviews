9aa5f8ca9a0050b1232218b7ae3668627a80e105
Your Name <you@example.com>
2023-01-06 16:21:38 -0800

    API js files completed

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -22,7 +22,7 @@ router.get("/",async(req,res) => {
     }
 });
 
-router.get("/:id", async (req, res) =>{
+router.get("/:id", async (req, res) => {
     //retro fit this code to our db
     try {
         const gamesData = await Games.findByPk(req.params.id, {
@@ -30,7 +30,7 @@ router.get("/:id", async (req, res) =>{
                 model: Review,
                 attributes: ["id","review_body","review_date","user_id"],
             },],
-        });
+        },);
         if (!gameData) {
             res.status(404).json({message:'No Game found with this id'});
             return;
@@ -38,5 +38,38 @@ router.get("/:id", async (req, res) =>{
         res.status(200).json(gameData);
     }catch (err) {
         res.status(500).jason(err);
+}
 });
+
+router.post("/", async (req, res) => {
+    try {
+      console.log("---------------POSTING--------------");
+      console.log(req.body);
+      const gamesData = await Games.create(req.body, {
+        title: req.body.title,
+        slug: req.body.slug,
+        game_description: req.body.game_description,
+        release_date: req.body.release_date,
+        metacritic: req.body.metacritic,
+        background_image: req.body.background_image
+      });
+      res.status(200).json("Nice job. You added to the DB");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  // delete user
+  router.delete("/:id", async (req, res) => {
+    try {
+      const userData = await Games.destroy({ where: { id: req.params.id } });
+      if (!userData) {
+        res.status(404).json("User Not Found");
+      }
+      res.status(200).json("Succesfully Deleted!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  module.exports = router;
+
 
