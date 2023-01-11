c96a93eb6fc4c85001ea10c11447f51b79f10fe0
Connor McGrath <connorjmcgrath93@gmail.com>
2023-01-10 21:46:42 -0800

    Fixed secret

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.secret,
+  secret: 'secret saucer',
   cookie: {
     maxAge: 300000,
     httpOnly: true,
